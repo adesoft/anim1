798ad02914ad8179dc7ee2deff08d0b9f0677bc1
adesoft <noreply>
2016-03-12 12:21:02 +0000

    Add component componente1

---
+++ ui/componente1.reel/componente1.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/componente1.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Componente1
+ * @extends Component
+ */
+exports.Componente1 = Component.specialize(/** @lends Componente1# */ {
+    constructor: {
+        value: function Componente1() {
+            this.super();
+        }
+    }
+});
